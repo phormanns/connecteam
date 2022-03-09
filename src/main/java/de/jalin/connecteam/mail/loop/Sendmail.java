9ec31491f222952c6e4563c862baeba09ec9694b
Peter Hormanns <peter.hormanns@jalin.de>
2022-03-09 19:15:09 +0100

    refactor smtp send

--- src/main/java/de/jalin/connecteam/mail/loop/Sendmail.java
+++ src/main/java/de/jalin/connecteam/mail/loop/Sendmail.java
@@ -2,8 +2,12 @@ package de.jalin.connecteam.mail.loop;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
@@ -37,7 +41,7 @@ public class Sendmail {
 	    this.random = new RandomIdent();
 	}
 	
-	public void send(final Post listPost) {
+	public void sendPost(final Post listPost) {
 		topic.getSubscriptions().forEach(new Consumer<Subscription>() {
 			@Override
 			public void accept(final Subscription subscription) {
@@ -57,7 +61,7 @@ public class Sendmail {
 		sendQueue.forEach(new Consumer<Post>() {
 			@Override
 			public void accept(Post mlPost) {
-				send(mlPost);
+				sendPost(mlPost);
 			}
 		});
 	}
@@ -65,69 +69,94 @@ public class Sendmail {
 	private void smtpSend(final Post mlPost, final String toAddress) throws CxException {
         try {
         	log.info("sending to " + toAddress);
-    		final String topicAddress = topic.getAddress();
+    		final String fromAddress = topic.getAddress();
     		final String subject = mlPost.getSubject();
     		final String content = mlPost.getTextContent();
     		final String originalFrom = mlPost.getOriginalFrom();
-			final MailAccount smtpAccount = topic.getSmtpAccount();
-			final String smtpHost = smtpAccount.getHost();
-			final int smtpPort = smtpAccount.getPort();
-			final String smtpLogin = smtpAccount.getLogin();
-			final String smtpPasswd = smtpAccount.getPasswd();
-			final AuthenticatingSMTPClient client = new AuthenticatingSMTPClient();
-			final String canonicalHostName = InetAddress.getLocalHost().getHostName();
-			client.connect(smtpHost, smtpPort);
-			int reply = client.getReplyCode();
-			if (!SMTPReply.isPositiveCompletion(reply)) {
-				log.error("smtp connect failed: " + smtpHost + ":" + smtpPort);
-			    throw new CxException("error_smtp_client_connect");
-			}
-			if (!client.execTLS()) {
-				log.error("smtp starttls failed: " + smtpHost + ":" + smtpPort);
-			    throw new CxException("error_smtp_client_starttls");
-			}
-			if (!client.elogin(canonicalHostName)) {
-				log.error("smtp ehlo failed: " + smtpHost + ":" + smtpPort);
-			    throw new CxException("error_smtp_client_ehlo");
-			}
-			if (!client.auth(AUTH_METHOD.LOGIN, smtpLogin, smtpPasswd)) {
-				log.error("smtp login failed: " + smtpLogin + "@" + smtpHost + ":" + smtpPort);
-			    throw new CxException("error_smtp_client_login");
-			}
-			client.setSender(topicAddress.trim());
-			client.addRecipient(toAddress.trim());
+			final AuthenticatingSMTPClient client = handleSMTPDialog(fromAddress, toAddress);
 			final Writer messageWriter = client.sendMessageData();
 			if (messageWriter == null) {
+				log.error("error_sending_email_relay");
 			    throw new CxException("error_sending_email_relay");
 			}
 			try (final PrintWriter printWriter = new PrintWriter(messageWriter)) {
-			    final SimpleSMTPHeader header = new SimpleSMTPHeader(topicAddress, toAddress, subject);
-			    header.addHeaderField("Content-Type", "text/plain; charset=ISO-8859-1");
-			    header.addHeaderField("Content-Transfer-Encoding", "8bit");
-			    header.addHeaderField("Message-ID", "<" + random.nextIdent() + "-" + topicAddress + ">");
-			    printWriter.write(header.toString());
-			    printWriter.write("Nachricht von " + originalFrom + "\n");
-			    printWriter.write("an den Verteiler " + topicAddress + "\n\n");
+			    printWriter.write(makeSMTPHeaders(fromAddress, toAddress, subject));
+			    
+			    printWriter.write(makeTopicInfo(fromAddress, originalFrom));
 			    printWriter.write(content);
-			    printWriter.write("\n\nAnlagen:\n");
-			    final Collection<AttachmentPath> attachments = mlPost.getAttachments();
-			    for (AttachmentPath att : attachments) {
-			    	String webDomain = topic.getWebDomain();
-			    	String webDomainWithProtocol = "https://" + topic.getWebDomain();
-			    	if (webDomain.startsWith("localhost:")) {
-			    		webDomainWithProtocol = "http://" + topic.getWebDomain();
-			    	}
-					printWriter.write(att.getName() + " " + webDomainWithProtocol + "/att/" + mlPost.getRandom() + "/" + att.getFilename() + "\n");
-			    }
+			    printWriter.write(makeAttachmentsList(mlPost));
 			}
 			if (!client.completePendingCommand()) {
 			    throw new CxException("error_sending_email");
 			}
 			client.logout();
 			client.disconnect();
-		} catch (NumberFormatException | IOException | InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+		} catch (NumberFormatException | IOException | GeneralSecurityException e) {
 			throw new CxException(e);
 		}
     }
+
+	public String makeTopicInfo(final String fromAddress, final String originalFrom) {
+		final StringBuffer messageInfo = new StringBuffer("Nachricht von " + originalFrom + "\n");
+		messageInfo.append("an den Verteiler " + fromAddress + "\n\n");
+		final String info = messageInfo.toString();
+		return info;
+	}
+
+	private AuthenticatingSMTPClient handleSMTPDialog(final String fromAddress, final String toAddress)
+			throws UnknownHostException, SocketException, IOException, CxException, NoSuchAlgorithmException,
+			InvalidKeyException, InvalidKeySpecException {
+		final MailAccount smtpAccount = topic.getSmtpAccount();
+		final String smtpHost = smtpAccount.getHost();
+		final int smtpPort = smtpAccount.getPort();
+		final String smtpLogin = smtpAccount.getLogin();
+		final String smtpPasswd = smtpAccount.getPasswd();
+		final AuthenticatingSMTPClient client = new AuthenticatingSMTPClient();
+		final String canonicalHostName = InetAddress.getLocalHost().getHostName();
+		client.connect(smtpHost, smtpPort);
+		int reply = client.getReplyCode();
+		if (!SMTPReply.isPositiveCompletion(reply)) {
+			log.error("smtp connect failed: " + smtpHost + ":" + smtpPort);
+		    throw new CxException("error_smtp_client_connect");
+		}
+		if (!client.execTLS()) {
+			log.error("smtp starttls failed: " + smtpHost + ":" + smtpPort);
+		    throw new CxException("error_smtp_client_starttls");
+		}
+		if (!client.elogin(canonicalHostName)) {
+			log.error("smtp ehlo failed: " + smtpHost + ":" + smtpPort);
+		    throw new CxException("error_smtp_client_ehlo");
+		}
+		if (!client.auth(AUTH_METHOD.LOGIN, smtpLogin, smtpPasswd)) {
+			log.error("smtp login failed: " + smtpLogin + "@" + smtpHost + ":" + smtpPort);
+		    throw new CxException("error_smtp_client_login");
+		}
+		client.setSender(fromAddress.trim());
+		client.addRecipient(toAddress.trim());
+		return client;
+	}
+
+	private String makeAttachmentsList(final Post mlPost) {
+		final StringWriter writer = new StringWriter();
+		writer.write("\n\nAnlagen:\n");
+		final Collection<AttachmentPath> attachments = mlPost.getAttachments();
+		for (AttachmentPath att : attachments) {
+			String webDomain = topic.getWebDomain();
+			String webDomainWithProtocol = "https://" + webDomain;
+			if (webDomain.startsWith("localhost:")) {
+				webDomainWithProtocol = "http://" + webDomain;
+			}
+			writer.write(att.getName() + " " + webDomainWithProtocol + "/att/" + mlPost.getRandom() + "/" + att.getFilename() + "\n");
+		}
+		return writer.toString();
+	}
+
+	private String makeSMTPHeaders(final String fromAddress, final String toAddress, final String subject) {
+		final SimpleSMTPHeader header = new SimpleSMTPHeader(fromAddress, toAddress, subject);
+		header.addHeaderField("Content-Type", "text/plain; charset=ISO-8859-1");
+		header.addHeaderField("Content-Transfer-Encoding", "8bit");
+		header.addHeaderField("Message-ID", "<" + random.nextIdent() + "-" + fromAddress + ">");
+		return header.toString();
+	}
 	
 }
