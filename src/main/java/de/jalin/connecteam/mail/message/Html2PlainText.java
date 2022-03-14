b34b121c664f92cdcf47506b04f1383fecc34eb9
Peter Hormanns <peter.hormanns@jalin.de>
2022-03-14 13:27:26 +0100

    clean code, setup for javaee8, servlet4.0

--- src/main/java/de/jalin/connecteam/mail/message/Html2PlainText.java
+++ src/main/java/de/jalin/connecteam/mail/message/Html2PlainText.java
@@ -1,15 +1,9 @@
 package de.jalin.connecteam.mail.message;
 
-import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
 import org.jsoup.internal.StringUtil;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
-import org.jsoup.select.Elements;
 import org.jsoup.select.NodeTraversor;
 import org.jsoup.select.NodeVisitor;
 
@@ -28,31 +22,6 @@ import org.jsoup.select.NodeVisitor;
  * @author Jonathan Hedley, jonathan@hedley.net
  */
 public class Html2PlainText {
-
-    private static final String userAgent = "Mozilla/5.0 (jsoup)";
-    private static final int timeout = 5 * 1000;
-
-    public static void main(String... args) throws IOException {
-        Validate.isTrue(args.length == 1 || args.length == 2, "usage: java -cp jsoup.jar org.jsoup.examples.HtmlToPlainText url [selector]");
-        final String url = args[0];
-        final String selector = args.length == 2 ? args[1] : null;
-
-        // fetch the specified URL and parse to a HTML DOM
-        Document doc = Jsoup.connect(url).userAgent(userAgent).timeout(timeout).get();
-
-        Html2PlainText formatter = new Html2PlainText();
-
-        if (selector != null) {
-            Elements elements = doc.select(selector); // get each element that matches the CSS selector
-            for (Element element : elements) {
-                String plainText = formatter.getPlainText(element); // format that element to plain text
-                System.out.println(plainText);
-            }
-        } else { // format the whole doc
-            String plainText = formatter.getPlainText(doc);
-            System.out.println(plainText);
-        }
-    }
 
     /**
      * Format an Element to plain-text
